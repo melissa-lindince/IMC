9c76117ec9f374d446954c70d5547f069b6c8d8c
Meliss-Lindince <lindincemelissa@gmail.com>
2021-09-12 21:41:39 -0300

    add an image

--- js/index.js
+++ js/index.js
@@ -9,25 +9,38 @@ function calculateBMI(form) {
 
     function peoplerOver60() {
         let moreThan60 = age >= 60
-        if (imc <= 21.9 && moreThan60) imcText = 'Abaixo do peso'
-        if (imc > 22 && imc <= 26.9 && moreThan60) imcText = 'Peso adequado'
-        if (imc > 27 && moreThan60)  imcText = 'Acimado peso'
+        if (imc <= 21.9 && moreThan60) {
+            imcText = 'Abaixo do peso'
+            imgUnderWeight()
+        }else if (imc > 22 && imc <= 26.9 && moreThan60) {
+            imcText = 'Peso adequado'
+          
+        } else if (imc > 27 && moreThan60) {
+            imcText = 'Acimado peso'
+            
+        }  
 
         result.innerHTML = `Resultado: ${parseFloat(imc).toFixed(2)}  ${imcText}`;  
     }
 
     function peopleUnder60() {
         let lessThan60 = age < 60 
-        if (imc <= 16.90 && lessThan60) imcText = 'Muito abaixo do peso'
-        if (imc > 17 && imc <= 18.4 && lessThan60) imcText = 'Abaixo do peso'
-        if (imc >= 18.5 && imc <= 24.9 && lessThan60) imcText = 'Peso adequado'
-        if (imc >= 25 && imc <= 29.9 && lessThan60)  imcText = 'Acima do peso'
+        if (imc <= 16.90 && lessThan60){
+            imcText = 'Muito abaixo do peso'
+            imgUnderWeight()
+        } else if (imc > 17 && imc <= 18.4 && lessThan60) {
+            imcText = 'Abaixo do peso'
+            imgUnderWeight()
+        } else if (imc >= 18.5 && imc <= 24.9 && lessThan60) {
+            imcText = 'Peso adequado'
+        } else if (imc >= 25 && imc <= 29.9 && lessThan60) {
+            imcText = 'Acima do peso'
+        }  
         
         result.innerHTML = `Resultado: ${parseFloat(imc).toFixed(2)}  ${imcText}`; 
     }
 
     function obesity() {
-        
         if (imc >= 30 && imc <= 34.9) {
             imcText = 'Obesidade grau 1' 
             imgObesity()
@@ -43,10 +56,12 @@ function calculateBMI(form) {
     }
 
     function imgObesity() {
-        document.getElementById("img").src="img/imgObesity.gif"
-        
+        document.getElementById("img").src="img/imgObesity.gif" 
     }
-
+    
+    function imgUnderWeight() {
+        document.getElementById('img').src="img/img1.gif"
+    }
     
     peoplerOver60()
     peopleUnder60()
